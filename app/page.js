fcf162b1fa8d6826d319dfa21ff6392896f186e5
PRINCE MEHRA <mrgaiusjuliusceasar@gmail.com>
2025-11-26 12:35:32 -0700

    Make chat window taller
    - Set chat interface to fixed height of 700px
    - Remove h-screen constraint from parent containers
    - Page now scrollable with info/tech sections below
    - Chat window gets more prominence on the page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import ChatInterface from '../components/ChatInterface';
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
-      <div className="container mx-auto px-4 py-8 h-screen flex flex-col">
-        <div className="max-w-5xl mx-auto w-full flex flex-col flex-1">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-5xl mx-auto w-full">
           {/* Header */}
           <div className="text-center mb-6">
             <h1 className="text-4xl font-bold mb-2 text-gray-900 dark:text-white">
@@ -16,7 +16,7 @@ export default function Home() {
           </div>
 
           {/* Chat Interface */}
-          <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg flex-1 flex flex-col overflow-hidden">
+          <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg flex flex-col overflow-hidden h-[700px]">
             <ChatInterface />
           </div>
 
